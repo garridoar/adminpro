703bbe1d29be203a0001006bf514e972a98d9516
Ariel Ezequiel Garrido <garridoar@outlook.com>
2020-02-06 11:03:47 -0300

    Sección 8 terminada

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -12,10 +12,10 @@ const routes: Routes = [
     path: '',
     component: PagesComponent,
     children: [
-        { path: 'dashboard', component: DashboardComponent },
-        { path: 'progress', component: ProgressComponent },
-        { path: 'graficas1', component: Graficas1Component },
-        { path: 'account-settings', component: AccountSettingsComponent },
+        { path: 'dashboard', component: DashboardComponent, data: { titulo: 'Dashboard' } },
+        { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress' } },
+        { path: 'graficas1', component: Graficas1Component, data: { titulo: 'Gráficas' } },
+        { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajustes del tema' } },
         { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
     ]
 }
